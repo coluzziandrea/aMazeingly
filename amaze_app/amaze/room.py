b70e58c30397aef7da2ed860a014e21bfe6443e4
Andrea Coluzzi <a.coluzzi@mariniimpianti.it>
2021-03-04 17:42:49 +0100

    WIP room class

--- amaze_app/amaze/room.py
+++ amaze_app/amaze/room.py
@@ -1,4 +1,21 @@
 class Room:
 
-    def __init__(self):
-        pass
+    north = -1
+    south = -1
+    east = -1
+    west = -1
+
+    objects = []
+
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+
+    def hasAnyObject(self):
+        return len(self.objects) > 0
+
+    def addObject(self, val):
+        self.objects.append(val)
+
+    def collectTargetObjects(self, targets):
+        return list(filter(lambda x:  x in targets, self.objects))
--- amaze_app/tests/test_amaze/test_room.py
+++ amaze_app/tests/test_amaze/test_room.py
@@ -6,5 +6,38 @@ from amaze.room import Room
 
 
 def test_init():
-    room = Room()
+    room = Room(1, "Foo Room")
     assert room is not None
+    assert room.id == 1
+    assert room.name == "Foo Room"
+
+
+def test_getters_setters():
+    room = Room(1, "Foo Room")
+    assert room.north == -1
+    assert room.south == -1
+    assert room.east == -1
+    assert room.west == -1
+
+    room.north = 10
+    room.south = 20
+    room.east = 30
+    room.west = 40
+
+    assert room.north == 10
+    assert room.south == 20
+    assert room.east == 30
+    assert room.west == 40
+
+
+def test_objects():
+    room = Room(1, "Foo Room")
+
+    assert not room.hasAnyObject()
+
+    room.addObject("Foo Object")
+
+    assert room.hasAnyObject()
+
+    assert "Foo Object" in room.collectTargetObjects(
+        ["Foo Object", "Test Object"])
