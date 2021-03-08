3600131bb8eae64db01d5e9c0a7721478cf34574
Andrea Coluzzi <a.coluzzi@mariniimpianti.it>
2021-03-08 12:22:34 +0100

    delete scripts

--- scripts/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-echo "BUILD Script: Init..."
-echo "No Op: Python code does not need to be built"
-echo "BUILD Script: Finish"
--- scripts/tests.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-echo "TEST Script: Init..."
-python -m pytest /amaze_app/tests 
-
-echo "TEST Script: Finish"
