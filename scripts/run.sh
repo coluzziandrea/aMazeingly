400304efc95596a9c0b0507a8c8af5bff3c17184
Andrea Coluzzi <a.coluzzi@mariniimpianti.it>
2021-03-08 12:14:43 +0100

    fix EOL

--- scripts/run.sh
+++ scripts/run.sh
@@ -9,4 +9,4 @@ python -m amaze_app.amaze.app ./scripts/input_map_example_1.json 2 Knife 'Potted
 echo "RUN Script: Executing Example 2..." 
 python -m amaze_app.amaze.app ./scripts/input_map_example_2.json 4 Knife 'Potted Plant' Pillow
 
-echo "RUN Script: Finish."
+echo "RUN Script: Finish"
--- scripts/tests.sh
+++ scripts/tests.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 echo "TEST Script: Init..."
 python -m pytest /amaze_app/tests 
+
+echo "TEST Script: Finish"
