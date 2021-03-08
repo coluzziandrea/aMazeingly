57fbbe1acf1ab849d38ca874697e1828755e236d
Andrea Coluzzi <a.coluzzi@mariniimpianti.it>
2021-03-08 12:25:18 +0100

    fix EOL

---
+++ scripts/build.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+echo "BUILD Script: Init..."
+echo "No Op: Python code does not need to be built"
+echo "BUILD Script: Finish"
---
+++ scripts/tests.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo "TEST Script: Init..."
+python -m pytest /amaze_app/tests 
+
+echo "TEST Script: Finish"
